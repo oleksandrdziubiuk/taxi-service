265e1753e3af4d80c842578dd4d438b0f182d9e0
sanek98cv <73754789+sanek98cv@users.noreply.github.com>
2021-01-20 11:38:23 +0200

    Added implementation

--- src/main/java/com/internet/shop/service/CarService.java
+++ src/main/java/com/internet/shop/service/CarService.java
@@ -4,17 +4,7 @@ import com.internet.shop.model.Car;
 import com.internet.shop.model.Driver;
 import java.util.List;
 
-public interface CarService {
-    Car create(Car car);
-
-    Car get(Long id);
-
-    List<Car> getAll();
-
-    Car update(Car car);
-
-    boolean delete(Long id);
-
+public interface CarService extends GenericService<Car, Long> {
     void addDriverToCar(Driver driver, Car car);
 
     void removeDriverFromCar(Driver driver, Car car);
--- src/main/java/com/internet/shop/service/DriverService.java
+++ src/main/java/com/internet/shop/service/DriverService.java
@@ -1,16 +1,6 @@
 package com.internet.shop.service;
 
 import com.internet.shop.model.Driver;
-import java.util.List;
 
-public interface DriverService {
-    Driver create(Driver driver);
-
-    Driver get(Long id);
-
-    List<Driver> getAll();
-
-    Driver update(Driver driver);
-
-    boolean delete(Long id);
+public interface DriverService extends GenericService<Driver, Long> {
 }
